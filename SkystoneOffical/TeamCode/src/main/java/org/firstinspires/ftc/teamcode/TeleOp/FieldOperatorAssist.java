2f7bd8ef0b551dcfea981fab83be3a894fe95c80
jabbate19 <josephabbateny@gmail.com>
2019-12-21 17:02:21 -0500

    Op Assist

--- SkystoneOffical/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FieldOperatorAssist.java
+++ SkystoneOffical/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FieldOperatorAssist.java
@@ -51,9 +51,9 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="TeleOp Field", group="Mecanum")
+@TeleOp(name="TeleOp Field Op Assist", group="Mecanum")
 
-public class Field extends OpMode
+public class FieldOperatorAssist extends OpMode
 {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
